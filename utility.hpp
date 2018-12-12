7918e9f088d8562fc02952316c2e60b70bb81d33
Anthony Wilson <wilsonan@us.ibm.com>
2018-12-12 16:10:35 -0600

    chassiskill: Add chassiskill tool
    This tool is meant to circumvent the d-bus method of
    powering down the chassis and directly pull the power
    from it. Meant for use by the 'obmcutil' tool.
    
    Change-Id: If8e51acbb3b192097fa9efa504d6cfa48d2bee9d
    Signed-off-by: Anthony Wilson <wilsonan@us.ibm.com>

---
+++ utility.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <string>
+
+namespace chassiskill
+{
+namespace utility
+{
+
+/** @brief Set the value of a specified gpio
+ *
+ *  @param[in] gpioName - GPIO to set
+ *  @param[in] activeLow - is pin is active at low voltage
+ *  @param[in] asserted - is pin in active state
+ *
+ *  @return bool - success of setting the GPIO
+ */
+bool gpioSetValue(const std::string& gpioName, bool activeLow, bool asserted)
+{
+    /* TODO */
+    return true;
+}
+
+} // namespace utility
+} // namespace chassiskill
